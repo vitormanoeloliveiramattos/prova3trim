800db473edfc9675cca363d31c79554005204900
vitormanoeloliveiramattos <130366864+vitormanoeloliveiramattos@users.noreply.github.com>
2023-10-23 11:41:22 -0300

    Add files via upload

---
+++ AjudandoRafaMoreira/frontEnd3trimLeoncio-main/calculo.js
@@ -0,0 +1,9 @@
+function Calcular(){
+
+    <script>
+    function calculateDogAge() {
+        const humanAge = document.getElementById('humanAge').value;
+        const dogAge = humanAge * 7;
+        document.getElementById('dogAge').textContent = dogAge + ' anos de idade canina.';
+    }
+</script>}
